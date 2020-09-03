2d440a4e908bb90edd37eafda78b4356f5711cb0
Per Egertun <per.egertun@chasacademy.se>
2020-09-03 16:58:19 +0200

    initial commit with project enviroment

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+require("dotenv").config();
+
+console.log(process.env.API_KEY);
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
